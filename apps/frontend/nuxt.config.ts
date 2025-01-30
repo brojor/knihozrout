c2dfb278540a43d57576dfc281f83ac2c7285bc3
brojor <bronislav.jordan@gmail.com>
2025-01-30 19:31:36 +0100

    style: formatting code

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,14 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
   modules: [
-    '@nuxt/eslint'
+    '@nuxt/eslint',
   ],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-11-01',
   eslint: {
     config: {
       stylistic: true,
-      standalone: false
-    }
-  }
+      standalone: false,
+    },
+  },
 })
