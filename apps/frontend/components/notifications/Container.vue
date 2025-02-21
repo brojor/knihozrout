473cf4ff39e1c18c9b02b7d6c57e989c437f7d29
brojor <bronislav.jordan@gmail.com>
2025-02-21 13:36:16 +0100

    refactor(frontend): Improve notifications transition

--- apps/frontend/components/notifications/Container.vue
+++ apps/frontend/components/notifications/Container.vue
@@ -1,3 +1,4 @@
+<!-- components/notifications/Container.vue -->
 <script setup lang="ts">
 import { useNotificationsStore } from '@/stores/notifications'
 
@@ -7,20 +8,21 @@ const store = useNotificationsStore()
 <template>
   <Teleport to="#teleports">
     <div class="fixed left-0 right-0 top-0 z-50 mx-auto max-w-lg px-4 pt-4">
-      <TransitionGroup
-        enter-active-class="transition duration-300 ease-out"
-        enter-from-class="transform -translate-y-4 opacity-0"
-        enter-to-class="transform translate-y-0 opacity-100"
-        leave-active-class="transition duration-200 ease-in"
-        leave-from-class="transform translate-y-0 opacity-100"
-        leave-to-class="transform -translate-y-4 opacity-0"
-      >
-        <NotificationsItem
-          v-for="notification in store.activeNotifications"
-          :key="notification.id"
-          :notification="notification"
-        />
-      </TransitionGroup>
+      <div class="relative">
+        <TransitionGroup
+          move-class="transition duration-500 ease"
+          enter-active-class="transition duration-500 ease"
+          leave-active-class="transition duration-500 ease absolute inset-x-0"
+          enter-from-class="transform -translate-y-4 opacity-0"
+          leave-to-class="transform -translate-y-4 opacity-0"
+        >
+          <NotificationsItem
+            v-for="notification in store.activeNotifications"
+            :key="notification.id"
+            :notification="notification"
+          />
+        </TransitionGroup>
+      </div>
     </div>
   </Teleport>
 </template>
